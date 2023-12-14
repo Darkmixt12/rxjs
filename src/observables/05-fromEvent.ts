db7a82e1bf2fef303da529f8a6de9c504da483e3
Steven Muñoz <darkmixt@gmail.com>
2023-12-14 12:21:00 -0600

    feat: range

---
+++ src/observables/05-fromEvent.ts
@@ -0,0 +1,23 @@
+import { fromEvent } from "rxjs";
+
+
+
+//! FROM EVENT nos permite crear obs en base a un event target+
+
+// EVENTOS DEL DOM
+
+const src1$ = fromEvent<MouseEvent>( document, 'click')
+const src2$ = fromEvent<KeyboardEvent>( document, 'keyup')
+
+const observer = {
+	next: val => console.log('next', val)
+}
+
+src1$.subscribe( ({x,y}) =>{
+	console.log( x, y)
+})
+
+
+src2$.subscribe( event =>{
+	console.log( event.key)
+})
